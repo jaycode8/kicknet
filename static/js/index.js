72dfa97c952a3961727d8e036b8de2ddf6e997c2
jaycode8 <jamesmumo443@gmail.com>
2024-06-27 14:23:20 +0300

    updates

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,12 @@
 $("document").ready(function () { 
     $(".menubar").click(function () {
-        $(".menuItem").toggleClass("activeBar")
-    })
+        // $(".menuItem").toggleClass("activeBar")
+        // alert("hey")
+        $(".menuItem").removeClass("-left-full");
+    });
+    $(".closemenubar").click(function () {
+        // $(".menuItem").toggleClass("activeBar")
+        // alert("hey")
+        $(".menuItem").addClass("-left-full");
+    });
 });
